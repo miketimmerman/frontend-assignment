d9ff4919a24a2e568dfa6a4d42a26ab1307d892d
miketimmerman <mike.timmerman@budgetthuis.nl>
2024-11-30 10:10:54 +0100

    format date and hover with link

--- src/components/FlightList.tsx
+++ src/components/FlightList.tsx
@@ -1,5 +1,7 @@
 import { Flight } from "@/types/flight";
 import Card from "./ui/Card";
+import { format } from "date-fns/format";
+import ChevronRight from "./icons/ChevronRight";
 
 type FlightListProps = {
   flights: Flight[];
@@ -8,30 +10,45 @@ type FlightListProps = {
 const FlightList = ({ flights }: FlightListProps) => (
   <div className="flex flex-col gap-4">
     {flights.map((flight) => (
-      <Card key={flight.flightIdentifier}>
-        <div className="text-xl font-bold">{flight.airport}</div>
-        <div className="grid grid-cols-4">
-          <div>
-            <p className="font-bold">Vluchtnummer</p>
-            <p>{flight.flightNumber}</p>
+      <a
+        href={`https://www.schiphol.nl${flight.url}`}
+        key={flight.flightIdentifier}
+        className="group"
+      >
+        <Card>
+          <div className="flex flex-row justify-between gap-4">
+            <div className="w-full">
+              <div className="text-xl font-bold group-hover:underline mb-2">
+                {flight.airport}
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <div>
+                  <p className="font-bold">Vluchtnummer</p>
+                  <p>{flight.flightNumber}</p>
+                </div>
+                <div>
+                  <p className="font-bold">Datum</p>
+                  <p>{format(new Date(flight.date), "dd-MM-yyyy")}</p>
+                </div>
+                <div>
+                  <p className="font-bold">Vetrektijd</p>
+                  <p>
+                    {flight.expectedTime}{" "}
+                    {flight.originalTime !== flight.expectedTime && (
+                      <span className="text-schiphol-grey-500 line-through">
+                        {flight.originalTime}
+                      </span>
+                    )}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-end group-hover:translate-x-1 transition-transform duration-300">
+              <ChevronRight />
+            </div>
           </div>
-          <div>
-            <p className="font-bold">Datum</p>
-            <p>{flight.date}</p>
-          </div>
-          <div>
-            <p className="font-bold">Vetrektijd</p>
-            <p>
-              {flight.expectedTime}{" "}
-              {flight.originalTime !== flight.expectedTime && (
-                <span className="text-schiphol-grey-500 line-through">
-                  {flight.originalTime}
-                </span>
-              )}
-            </p>
-          </div>
-        </div>
-      </Card>
+        </Card>
+      </a>
     ))}
   </div>
 );

c19f32a7c8a6817db50c577cb86f020b2400de3e
miketimmerman <mike.timmerman@budgetthuis.nl>
2024-11-30 10:47:28 +0100

    move inline functions

--- src/components/Departures.tsx
+++ src/components/Departures.tsx
@@ -9,6 +9,14 @@ const Departures = () => {
   const { search, state, flights, setSearch } = useDepartures();
   const [sortDirection, setSortDirection] = useState<"asc" | "desc">("asc");
 
+  const handleSortDirection = () => {
+    setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+  };
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
   return (
     <div className="container mx-auto px-4 flex flex-col gap-4">
       <h1 className="text-5xl font-bold text-schiphol-blue my-10 text-center">
@@ -18,14 +26,12 @@ const Departures = () => {
         <Input
           placeholder="Bijvoorbeeld Amsterdam"
           value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          onChange={handleSearchChange}
         />
         <Button
           className="sm:w-48 shrink-0 [&:disabled]:bg-gray-400 [&:disabled]:cursor-not-allowed flex items-center justify-between gap-2"
           disabled={state !== "success"}
-          onClick={() =>
-            setSortDirection(sortDirection === "asc" ? "desc" : "asc")
-          }
+          onClick={handleSortDirection}
         >
           Sorteer{" "}
           {sortDirection === "asc" ? (
@@ -40,7 +46,7 @@ const Departures = () => {
         </Button>
       </div>
       {state === "loading" && <p>Informatie wordt opgehaald...</p>}
-      {state === "notFound" && <p>Geen resultaten gevonden</p>}
+      {state === "notFound" && <p>Geen vluchten gevonden</p>}
       {state === "success" && (
         <FlightList flights={flights} sortDirection={sortDirection} />
       )}
